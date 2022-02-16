409f7b51f42bb4b473b67d2cf6f634873f2ec7b6
biareiam <biareiam@gmail.com>
2022-02-16 20:40:01 +0000

    creating the home page and the article page

--- virtualbookshelf2022/models.py
+++ virtualbookshelf2022/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post (models.Model):
+    title = models.CharField(max_length=255)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=255)
+    #featured_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    #featured_image = CloudinaryField('image', default='placeholder')
+    #body = RichTextField()
+    body = models.TextField()
+    category = models.CharField(max_length=255, default="Romance")
+    likes = models.ManyToManyField(User, related_name="blog_posts")
+    
+    
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.username)
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+    def total_likes(self):
+        return self.likes.count()
+
+

c9685dbf0777b71bc13f188641a993d5f2628a3e
biareiam <biareiam@gmail.com>
2022-02-16 23:22:24 +0000

    starting the profile page

--- virtualbookshelf2022/urls.py
+++ virtualbookshelf2022/urls.py
@@ -3,11 +3,11 @@ from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, Del
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path ('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
-    path ('add_post/', AddPostView.as_view(), name="add_post"),
-    path ('add_genres/', AddCategoryView.as_view(), name="add_genre"),
-    path ('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
-    path ('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name="add_post"),
+    path('add_genres/', AddCategoryView.as_view(), name="add_genre"),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
     path('category/<str:cats>', CategoryView, name="category"),
     path('category-list/', CategoryListView, name="category-list"),
     path('like/<int:pk>', LikeView, name='like_post'),
